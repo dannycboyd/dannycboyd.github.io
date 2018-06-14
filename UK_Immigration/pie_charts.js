70f1ba951146f73871032f1e45dc5b9aad73797f
Ryan Oliver <roliver399@gmail.com>
2018-06-14 00:57:13 -0700

    two rows of pie charts, colors semi okay, need another legend ccause the catgories diverge too much between stack & pie, need to size down 2nd row of pie charts

--- UK_Immigration/pie_charts.js
+++ UK_Immigration/pie_charts.js
@@ -4,8 +4,6 @@ var Promise;
 var console;
 var csv = Promise.promisify(d3.csv);
 
-
-var pie_data;
 var chart_data$ = csv('immigrant_population_data.csv');
 
 var pie_width = 250,
@@ -20,11 +18,18 @@ var pie_margin = {
 };
 
 var percent_format = d3.format('.2%');
+var percent_format_1 = d3.format('.1%');
 var comma_format = d3.format(",");
 
 var pie_color = d3.scaleOrdinal()
     .domain(["United Kingdom", "Sub-Saharan Africa", "EU 8",    "EU 14",   "South Asia", "Other"])
-    .range( ["#00247D",         "#AEC7E8",            "#8C564B", "#9467BD", "#D62728",    "#C7C7C7" ]);
+    .range( ["#00247D",        "#AEC7E8",            "#8C564B", "#9467BD", "#D62728",    "#C7C7C7" ]);
+
+pie_color2 = d3.scaleOrdinal()
+    .domain(["United Kingdom","EU 14"  ,"EU 8"   ,"EU 2"   ,"EU Other","Europe Other","Middle East and Central Asia","East Asia","South Asia","South East Asia","Sub-Saharan Africa","North Africa","North America","Central and South America","Oceania","Other"])
+//.range([ "#00247D"       ,"#9467BD","#8C564B","       ","#7095ef" ,"#5041f4"     ,"#7228a3"                     ,"#d13a3a"  ,"#ff4242"   ,"#f96b52"        ,"#f4a742"           ,"       "     ,"       "      ,"       "                  ,"       "])
+  .range([ "#00247D"       ,"#094ef7","#0090ff","#49d8fc","#92E7FC" ,"#5041f4"     ,"#7228a3"                     ,"#d13a3a"  ,"#ff4242"   ,"#f96b52"        ,"#FFEE0A"           ,"#f4a742"     ,"#13c425"      ,"#79c94a"                  ,"#f23acd"  ,"#C7C7C7"])
+    ;
 
  var pie = d3.pie()
         //.sort(null)
@@ -45,7 +50,6 @@ Promise.all([chart_data$]) // Flatten promises, read data
         }
     }
     
-    pie_data = chart_data;
     pie_chart(chart_data,0);
     pie_chart(chart_data,6);
     pie_chart(chart_data,12);
@@ -69,40 +73,40 @@ function pie_chart(data, index) {
     //console.log(total);
     //p1_data.push(0)
     var col_name;
-    var other = {category: 'Other', percent: 0, year: row["Year"]};
+    var non_uk_percent = 0;
+    var other = {category: 'Other', percent: 0/*, year: row["Year"]*/};
     for (var col in data.columns){
         col_name=data.columns[col];        
         if (col_name === "EU 14" ||
             col_name === "EU 8" ||
             col_name === "South Asia" ||
-            col_name === "Sub-Saharan Africa" ||
-            col_name === "United Kingdom"
+            col_name === "Sub-Saharan Africa"            
             ){   
             p1_data.push({category: col_name,
-                        percent:row[col_name]/total,
-                        //year: row["Year"]
-                         })
+                        percent:row[col_name]/total})
+            p2_data.push({category: col_name,
+                        percent:row[col_name]/total})
+            non_uk_percent += row[col_name]/total
+        }else if (col_name === "United Kingdom"){
+            p1_data.push({category: col_name,
+                        percent:row[col_name]/total})
         }else if (col_name != "Year" && col_name != "Total"){
+            non_uk_percent += row[col_name]/total
             other.percent = other.percent + row[col_name]/total;
+            p2_data.push({category: col_name,
+                        percent:row[col_name]/total})
         }
     }p1_data.push(other);
-
+    console.log(p1_data);
     var arc = d3.arc()
         .outerRadius(radius * 0.5)
-        .innerRadius(radius * 0.175);
+        .innerRadius(radius * 0.0);
 
-    var outerArc = d3.arc()
-        .outerRadius(radius * 0.9)
-        .innerRadius(radius * 0.9);
-
-   
-    
     //gonna have to do some dynamic posititioning
     var svg = d3.select('#pie_charts')
-    //.select("body").append("svg")
         .append('svg')
         .attr("id","pie")
-        .attr("display", "inline-block")
+        //.attr("display", "inline-block")
         .attr("width", pie_width+pie_margin.right + pie_margin.left)
         .attr("height", pie_height)
       .append("g")
@@ -112,21 +116,22 @@ function pie_chart(data, index) {
         .data(pie(p1_data))
       .enter().append("g")
         .attr("class", "arc");
-
+    
     var tooltip = d3.select('#pie_charts')
         .append('div')                             
-        .attr('class', 'tooltip');                 
+        .attr('class', 'tooltip')
+        .style('font-family', 'monospace');                 
 
     tooltip.append('div')                        
         .attr('class', 'category')
         //.style('display','table-cell')
-        .style('font-family', 'monospace')
+        //.style('font-family', 'monospace')
         //.style('display','inline-block')
         ;
 
     tooltip.append('div')                        
         .attr('class', 'count')
-        .style('font-family', 'monospace')
+        //.style('font-family', 'monospace')
         //.style('display','table-cell')
         //.style('display','inline-block')
     ;
@@ -138,17 +143,119 @@ function pie_chart(data, index) {
         //.style('display','inline-block')
     ;
     
-    g.append("path")
+    g.append("text")
+        .attr("text-anchor", "middle")
+        .attr('font-size', '20em')
+        .attr('y', 125)
+        .text(row["Year"]);
+    
+    g.append("text")
+        .attr("text-anchor", "middle")
+        .style('font-size', '1.25em')
+        .attr('x', 105)
+        .attr('y', 80)
+        .text(percent_format_1(p1_data[0].percent));
+    
+    g.append("text")
+        .attr("text-anchor", "middle")
+        .style('font-size', '1.25em')
+        .attr('x', -65)
+        .attr('y', -100)
+        .text(percent_format_1(non_uk_percent));
+    
+     g.append("path")
         .data(pie(p1_data))
         .attr("d", arc)
         .style("fill", function(d) { 
             //console.log(d);
-            return pie_color(d.data.category); })
+            return pie_color2(d.data.category); })
         .on('mouseover', function(d) {
-            console.log(d);
+            //console.log(d);
             tooltip.select('.category').html("Origin&nbsp&nbsp&nbsp&nbsp&nbsp" + ": " + d.data.category);
             tooltip.select('.count').html("Population&nbsp" + ": "+ comma_format(+(d.data.percent*total*1000)));
-            tooltip.select('.percent').html( "Percent&nbsp&nbsp&nbsp&nbsp: " + percent_format(d.data.percent));
+            tooltip.select('.percent').html( "Percent&nbsp&nbsp&nbsp&nbsp"+": " + percent_format(d.data.percent));
+            tooltip.style('display', 'block');
+        })
+        .on('mouseout', function() {tooltip.style('display', 'none');})
+        .on('mouseout', function() {tooltip.style('display', 'none');})
+        .on('mousemove', function(d) {
+			tooltip.style('top', (d3.event.layerY + 10) + 'px')
+			.style('left', (d3.event.layerX - 25) + 'px');
+		})
+        pie_chart_row2(p2_data);
+        ;
+    
+function pie_chart_row2(p1_data) {
+    //console.log("row2call")
+    //console.log(p2_data);
+    
+    
+    var arc = d3.arc()
+        .outerRadius(radius * 0.5)
+        .innerRadius(radius * 0.0000001)
+    ;
+
+    //gonna have to do some dynamic posititioning
+    var svg = d3.select('#pie_charts_2')
+        .append('svg')
+        .attr("id","pie")
+        //.attr("display", "inline-block")
+        .attr("width", pie_width+pie_margin.right + pie_margin.left)
+        .attr("height", pie_height)
+      .append("g")
+        .attr("transform", "translate("+ pie_width/2 +","+ pie_height/2 +")")
+        ;
+
+    var g = svg.selectAll(".arc")
+        .data(pie(p1_data))
+      .enter().append("g")
+        .attr("class", "arc");
+    
+    var tooltip = d3.select('#pie_charts')
+        .append('div')                             
+        .attr('class', 'tooltip')
+        .style('font-family', 'monospace');                 
+
+    tooltip.append('div')                        
+        .attr('class', 'category')
+        //.style('display','table-cell')
+        //.style('font-family', 'monospace')
+        //.style('display','inline-block')
+        ;
+
+    tooltip.append('div')                        
+        .attr('class', 'count')
+        //.style('font-family', 'monospace')
+        //.style('display','table-cell')
+        //.style('display','inline-block')
+    ;
+
+    tooltip.append('div')                        
+        .attr('class', 'percent')
+        .style('font-family', 'monospace')
+        //.style('display','table-cell')
+        //.style('display','inline-block')
+    ;
+    
+    /*
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr('font-size', '20em')
+        .attr('y', 5)
+        .text(row["Year"]);
+*/
+    
+     g.append("path")
+        .data(pie(p1_data))
+        .attr("d", arc)
+        .style("fill", function(d) { 
+            //console.log(d);
+            return pie_color2(d.data.category); })
+        .on('mouseover', function(d) {
+            //console.log(d);
+            tooltip.select('.category').html("Origin&nbsp&nbsp&nbsp&nbsp&nbsp" + ": " + d.data.category);
+            tooltip.select('.count').html("Population&nbsp" + ": "+ comma_format(+(d.data.percent*total*1000)));
+            tooltip.select('.percent').html( "Percent&nbsp&nbsp&nbsp&nbsp"+": " + percent_format(d.data.percent));
             tooltip.style('display', 'block');
         })
         .on('mouseout', function() {tooltip.style('display', 'none');})
@@ -158,20 +265,13 @@ function pie_chart(data, index) {
 			.style('left', (d3.event.layerX - 25) + 'px');
 		})
         ;
-    g.append("text")
-        .attr("text-anchor", "middle")
-        .attr('font-size', '20em')
-        .attr('y', 5)
-        .text(row["Year"]);
+
     /*
     g.append("text")
         .attr('y', 15)
         .text("Population");
     */
-    svg.append("g")
-        .attr("class", "labels");
-           
-    svg.append("g")
-        .attr("class", "lines");
+    }
+    
     
 }
