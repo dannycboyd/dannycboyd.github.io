273dfad8c02682edcd07fdda4ba15a90daf60906
Danny Boyd <dacboyd@ucsc.edu>
2018-06-21 20:31:29 -0700

    gen now has mouse movement detection

--- gen/draw.js
+++ gen/draw.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext('2d');
 
 var height = 0;
 var width = 0;
-var arbitrary = 0;
+var arbitrary = 150;
 var center = {x: 0, y: 0};
 var mouse = {x: 0, y: 0};
 var maxSize = 100;
@@ -27,48 +27,37 @@ function resizeCanvas() {
     width = canvas.width;
     center.x = width / 2;
     center.y = height / 2;
-    arbitrary = (height + width) / 2;
 
-        /**
-         * Your drawings need to be inside this function otherwise they will be reset when
-         * you resize the browser window and the canvas goes will be cleared.
-         */
         drawStuff();
 }
 window.addEventListener('resize', resizeCanvas, false);
 resizeCanvas();
 
-window.addEventListener('mouseMove', e => {
-    console.log(e);
-
+canvas.addEventListener('mousemove', e => {
     mouse.x = e.layerX;
     mouse.y = e.layerY;
     drawStuff();
 });
 window.addEventListener('click', e => {
-    console.log(e);
     mouse.x = e.layerX;
     mouse.y = e.layerY;
     drawStuff();
 })
 
 function drawStuff() {
-    // console.log(squares);
     if(squares) {
         ctx.clearRect(0, 0, width, height);
         squares.forEach((_square) => {
             let color = colorPos(_square)
             ctx.fillStyle = color;
-            square(_square.x * width, _square.y * height, _square.size);
+            square(_square);
         })
     }
 }
 
 function dist(sq) {
-    // console.log(sq, mouse);
-    let h = (Math.sqrt( Math.pow(sq.x * width - mouse.x, 2) + Math.pow(sq.y * height - mouse.y, 2) * 2));
-    console.log(h);
-    return h / arbitrary;
+    let a = (Math.sqrt( Math.pow(sq.x * width - mouse.x, 2) + Math.pow(sq.y * height - mouse.y, 2) * 2)) / arbitrary;
+    return a;
 }
 
 function colorPos(sq) {
@@ -76,14 +65,17 @@ function colorPos(sq) {
     let g = Math.round(sq.y * 255);
     let b = Math.round(sq.size);
     let a = dist(sq);
-    // console.log(r, g, b, a);
-    return `rgba(${r}, ${g}, ${b}, ${a})`
+    return `rgba(${r}, ${g}, ${b}, ${1})`
 }
 
-function square(x, y, s, s2 = null) {
-    if (!s2) {
-        s2 = s;
-    }
-    // console.log(x, y, s);
-    ctx.fillRect(x - (s/2), y - (s2/2), s, s2);
+function square(sq) {
+//    if (!s2) { s2 = s; }
+//    console.log(sq);
+    let ratio = dist(sq);
+//    console.log(ratio);
+    let side = ratio > 1 ? sq.size : ratio * sq.size;
+//    let side = sq.size - (dist(sq) * sq.size);
+//    console.log(sq, side);
+    
+    ctx.fillRect((sq.x * width) - (side/2), (sq.y * height) - (side/2), side, side);
 }
