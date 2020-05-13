a659ed28d679c82dbe01d4aa20fe4716477ba32e
Danny Boyd <dacboyd@ucsc.edu>
2020-05-13 01:04:42 -0700

    async generateLinks.js

--- recipes/generateLinks.js
+++ recipes/generateLinks.js
@@ -2,32 +2,37 @@
 
 console.log(__dirname);
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const dataPath = __dirname + '/data';
-const outfile = fs.openSync(__dirname + '/allFiles.json', 'w');
 
-const files = fs.readdirSync(dataPath, { withFileTypes: true });
-const outData = [];
-for (let file of files) {
-  if (/.*\.json/.test(file.name)) {
-    console.log(file);
-    const data = fs.readFileSync(dataPath + '/' + file.name)
-    
-    // if (e) {
-    //   console.error(e);
-    //   return;
-    // } else {
-    // }
-    let recipeData = JSON.parse(data);
-    console.log(file.name, recipeData.name);
-    outData.push({
-      path: file.name,
-      name: recipeData.name,
-    });
+async function run() {
 
+  const outfile = await fs.open(__dirname + '/allFiles.json', 'w');
+  
+  const files = await fs.readdir(dataPath, { withFileTypes: true });
+  const outData = [];
+  for (let file of files) {
+    if (/.*\.json/.test(file.name)) {
+      console.log(file);
+      const data = await fs.readFile(dataPath + '/' + file.name)
+      
+      // if (e) {
+      //   console.error(e);
+      //   return;
+      // } else {
+      // }
+      let recipeData = JSON.parse(data);
+      console.log(file.name, recipeData.name);
+      outData.push({
+        path: file.name,
+        name: recipeData.name,
+      });
+  
+    }
   }
+  
+  console.log('writing the files');
+  await fs.writeFile(outfile, Buffer.from(JSON.stringify(outData)));
 }
-
-console.log('writing the files');
-fs.writeFileSync(outfile, Buffer.from(JSON.stringify(outData)));
+run();
