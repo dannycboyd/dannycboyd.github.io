3e8ef2a92a0c9dbdd4172aae1a04879042e62665
Danny Boyd <odannyboyd@montara.com>
2021-01-24 20:23:05 -0800

    added editing modes. Line style options, saving

--- photocomp/src/photocomp.js
+++ photocomp/src/photocomp.js
@@ -2,6 +2,19 @@ let pathData = [];
 let pathNodes = [];
 let images = []; // { x, y, d }
 let svgNode = d3.select('svg');
+let count = 0;
+
+let strokeColor = 'black';
+d3.select('select#color').on('change', function (e) {
+  console.log(e.target.value);
+  strokeColor = e.target.value;
+})
+
+let strokeWidth = 1;
+d3.select('input#strokeWidth')
+  .on('change', function (e) {
+    strokeWidth = e.target.value;
+  });
 
 var CLICK_DISTANCE = 4,
   CLICK_DISTANCE_2 = CLICK_DISTANCE * CLICK_DISTANCE;
@@ -37,25 +50,47 @@ function drawImages() {
     .attr('x', d => d.x)
     .attr('y', d => d.y)
     .attr('xlink:href', d => d.data)
-    .attr('id', d => d.id)
-    .call(d3.drag()
-      .clickDistance(CLICK_DISTANCE)
-      .on("start", function (event, d) {
-        event.sourceEvent.preventDefault();
-        console.log('aaaa');
-        d3.selectAll('image.active').attr('class', null);
-        d3.select(this).attr('class', 'active');
-      })
-      .on("drag", function (event, d) {
-        event.sourceEvent.preventDefault();
-        console.log(event, d);
-        d3.select(this).raise().attr('x', d.x = event.x).attr('y', d.y = event.y);
-      })
-      .on("end", (event, d) => {
-        event.sourceEvent.preventDefault();
-        console.log(event, d);
-      }));
+    .on('click', (d) => { console.log(d) })
+    // .on('mousemove', (d) => { console.log(d) })
+    .attr('id', d => d.id);
 }
+d3.select('button#pointer').on('click', imagesDraggable);
+function imagesDraggable() {
+  svgNode.selectAll('image')
+    .call(dragHandler);
+
+  svgNode.on('mousedown', null);
+}
+
+d3.select('button#pen').on('click', imagesStatic);
+function imagesStatic() {
+  svgNode.selectAll('image')
+    .on('click', null)
+    .on('mousedown.drag', null)
+  svgNode.on('mousedown', svgPaint)
+}
+imagesStatic();
+
+let dragEmpty = d3.drag()
+  .on('drag', function () { })
+
+let dragHandler = d3.drag()
+  .clickDistance(CLICK_DISTANCE)
+  .on("start", function (event, d) {
+    event.sourceEvent.preventDefault();
+    console.log('aaaa');
+    d3.selectAll('image.active').attr('class', null);
+    d3.select(this).attr('class', 'active');
+  })
+  .on("drag", function (event, d) {
+    event.sourceEvent.preventDefault();
+    console.log(event, d);
+    d3.select(this).raise().attr('x', d.x = event.x).attr('y', d.y = event.y);
+  })
+  .on("end", (event, d) => {
+    event.sourceEvent.preventDefault();
+    console.log(event, d);
+  });
 
 let AddImage = d3.select('input#addImage')
   .on('change', (event) => {
@@ -103,41 +138,19 @@ function dragended(d) {
   d3.select(this).classed("active", false);
 }
 
-
-function imageMouseDown(event) {
-  event.stopPropagation();
-
-  console.log('imageMouseDown');
-  let node = d3.select(this)
-    .on('mousemove', imageMouseMove)
-    .on('mouseup', imageMouseUp)
-    .attr('class', 'active');
-
-  function imageMouseMove(event, d) {
-    d3.select(this).raise().attr('x', d.x = event.x)
-      .attr('y', d.y = event.y);
-  }
-
-  function imageMouseUp(event) {
-    node.on('mousemove', null)
-      .on('mouseup', null);
-  }
-}
-
-
 function dist(x1, y1, x2, y2) {
   console.log(x1, x2, y1, y2);
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 
-let line = d3.line().context(null);
-svgNode.on("mousedown", function (event) {
+function svgPaint(event) {
   let node = d3.select(this)
     .on('mousemove', mousemove)
     .on('mouseup', mouseup)
     .append('path')
     .attr('class', 'active')
-    .attr('stroke', 'black')
+    .attr('stroke', strokeColor)
+    .attr('stroke-width', strokeWidth)
     .attr('fill', 'none');
   pathNodes.push(node);
   pathData.push([event.layerX, event.layerY]);
@@ -164,4 +177,32 @@ svgNode.on("mousedown", function (event) {
 
     pathData = [];
   }
-})
+}
+
+let line = d3.line().context(null);
+d3.select('button#save').on('click', saveImage);
+function saveImage() { // http://bl.ocks.org/biovisualize/8187844
+  let svgString = new XMLSerializer().serializeToString(document.querySelector('svg'));
+  let canvas = document.querySelector('canvas');
+
+  let ctx = canvas.getContext('2d');
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight
+  let DOMURL = self.URL || self.webkitURL || self;
+
+  let img = new Image();
+  let svg = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
+  let url = DOMURL.createObjectURL(svg);
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+    var png = canvas.toDataURL('image/png');
+    // document.querySelector('#png-container').innterHTML = 
+    // save the URL here
+    let download = document.createElement('a');
+    download.href = png;
+    download.download = `image-result-${count}.png`;
+    count += 1;
+    download.click();
+  }
+  img.src = url;
+}
